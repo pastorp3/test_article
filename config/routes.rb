c443a5bdeb3925336e8aef7025b61f8c9dd30133
Jose Pedraza <pastorpedraza3@gmail.com>
2020-09-03 12:21:03 -0500

    Tagging feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  
+  root to: 'articles#index'
   resources :articles do
     resources :comments
   end
-  root to: 'articles#index'
+  resources :tags
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
